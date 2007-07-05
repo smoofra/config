39858bef423134540499d376f6807d83406deeab
tailor <larry@elder-gods.org>
2007-07-05 16:16:31 +0000

    [config @ change default font back]

--- init.el
+++ init.el
@@ -1445,7 +1445,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "#110023" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 133 :width normal :family "bitstream-bitstream vera sans mono"))))
+ '(default ((t (:stipple nil :background "#110022" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 133 :width normal :family "bitstream-bitstream vera sans mono"))))
  '(cursor ((t (:background "#ffffff"))))
  '(diff-changed ((nil (:foreground "yellow"))))
  '(diff-file-header ((((class color) (min-colors 88) (background dark)) (:foreground "cyan" :weight bold))))

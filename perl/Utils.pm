6252ccbeff101d3ac54e35285c4625ed9e71806e
Larry D'Anna <larry@melkor.(none)>
2008-02-15 18:31:01 -0500

    psg

--- perl/Utils.pm
+++ perl/Utils.pm
@@ -6,7 +6,9 @@ package Utils;
 
 require Exporter;
 @ISA = qw(Exporter);
-@EXPORT = qw(croak ssystem);
+@EXPORT = qw(slurp slurpdir croak ssystem);
+
+
 
 sub croak {
   if (scalar @_) {
@@ -15,6 +17,25 @@ sub croak {
   } else {
     die "$!\n";
   }
+}
+
+
+sub slurpdir {
+  my $dn = shift;
+  my $fh;
+  opendir $fh, $dn or croak;
+  my @x = grep {$_ ne '.' && $_ ne '..'} readdir $fh;
+  close $fh;
+  return @x;
+}
+ 
+sub slurp {
+  my $fn = shift;
+  my $fh;
+  open $fh, $fn or croak;
+  my $r = join '', <$fh>;
+  close $fh;
+  return $r;
 }
 
 sub ssystem  {

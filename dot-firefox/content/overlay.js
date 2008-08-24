68fc585f105919789557688d9bf4694f0f14e3b7
Larry D'Anna <larry@azazel.(none)>
2008-08-23 20:23:34 -0400

    finally google reader doesnt suck

--- dot-firefox/content/overlay.js
+++ dot-firefox/content/overlay.js
@@ -27,40 +27,77 @@ function tabRelativeSelect(delta)
 }
 
 
-function fookey(keyCode, type) { 
-	var doc = gBrowser.contentDocument; 
+function fookey(keyCode, charCode, type) { 
+	var doc = gBrowser.contentDocument.wrappedJSObject; 
+	//var elt = doc.getElementById("main"); 
+	var elt = doc; 
 	var e = doc.createEvent("KeyEvents"); 
-	e.initKeyEvent(type, true, true, doc.defaultView,
+	e.initKeyEvent(type, true, false, null, 
 				   false, false, false, false,
-				   keyCode, 0);
-	doc.dispatchEvent(e); 
+				   keyCode, charCode);
+	var cancelled = !elt.dispatchEvent(e); 
+	if (cancelled) 
+		alert("cancelled"); 
 }
 
-function fooup(event) {
-	var utils = window.QueryInterface(Components.interfaces.nsIInterfaceRequestor).getInterface(Components.interfaces.nsIDOMWindowUtils);
-// 	utils.sendKeyEvent("keydown",  KeyEvent["DOM_VK_UP"], 0, 0);
-// 	utils.sendKeyEvent("keypress", KeyEvent["DOM_VK_UP"], 0, 0);
-// 	utils.sendKeyEvent("keyup",    KeyEvent["DOM_VK_UP"], 0, 0);
-// 	utils.sendKeyEvent("keydown",  38, 0, 0);
-// 	utils.sendKeyEvent("keypress", 38, 0, 0);
-// 	utils.sendKeyEvent("keyup",    38, 0, 0);
-	fookey(KeyEvent["DOM_VK_UP"], "keyup"); 
-	fookey(KeyEvent["DOM_VK_UP"], "keypress"); 
-	fookey(KeyEvent["DOM_VK_UP"], "keyup"); 
 
+function fooup(event) {
+	event.stopPropagation(); 
+	//event.preventDefault(); 
+	fookey(KeyEvent["DOM_VK_UP"], 0,  "keyup"); 
+	fookey(KeyEvent["DOM_VK_UP"], 0,  "keypress"); 
+	fookey(KeyEvent["DOM_VK_UP"], 0,  "keyup"); 
 }
 
 
 function foodown(event) {
-	var utils = window.QueryInterface(Components.interfaces.nsIInterfaceRequestor).getInterface(Components.interfaces.nsIDOMWindowUtils);
-// 	utils.sendKeyEvent("keydown",  40, 0, 0);
-// 	utils.sendKeyEvent("keypress", 40, 0, 0);
-// 	utils.sendKeyEvent("keyup",    40, 0, 0);
-// 	utils.sendKeyEvent("keydown",  KeyEvent["DOM_VK_DOWN"], 0, 0);
-// 	utils.sendKeyEvent("keypress", KeyEvent["DOM_VK_DOWN"], 0, 0);
-// 	utils.sendKeyEvent("keyup",    KeyEvent["DOM_VK_DOWN"], 0, 0);
-	fookey(KeyEvent["DOM_VK_DOWN"], "keydown"); 
-	fookey(KeyEvent["DOM_VK_DOWN"], "keypress"); 
-	fookey(KeyEvent["DOM_VK_DOWN"], "keyup"); 
+	event.stopPropagation() ;
+	fookey(KeyEvent["DOM_VK_DOWN"], 0,  "keydown"); 
+	fookey(KeyEvent["DOM_VK_DOWN"], 0,  "keypress"); 
+	fookey(KeyEvent["DOM_VK_DOWN"], 0,  "keyup"); 
 }
+
+
+function fook(event) {
+	fookey(KeyEvent["DOM_VK_K"], 0,  "keydown"); 
+	fookey(KeyEvent["DOM_VK_K"], 0,  "keypress"); 
+	fookey(KeyEvent["DOM_VK_K"], 0,  "keyup"); 
+}
+
+
+function fooj(event) {
+	fookey(KeyEvent["DOM_VK_J"], 0,  "keydown"); 
+	fookey(KeyEvent["DOM_VK_J"], 0,  "keypress"); 
+	fookey(KeyEvent["DOM_VK_J"], 0,  "keyup"); 
+}
+
+function foolistener(event) { 
+	if (gBrowser.contentDocument.location.toString().indexOf("http://www.google.com/reader") == 0) {
+		if (event.altKey) {
+			if (String.fromCharCode(event.charCode) == "j")
+				fooj(event); 
+			else if (String.fromCharCode(event.charCode) == "k")
+				fook(event); 
+		} else {
+			if (String.fromCharCode(event.charCode) == "j")
+				foodown(event); 
+			else if (String.fromCharCode(event.charCode) == "k")
+				fooup(event); 
+		}
+	}
+}
+
+function normalj () { 
+	if (gBrowser.contentDocument.location.toString().indexOf("http://www.google.com/reader") != 0) 
+		goDoCommand('cmd_scrollLineDown');
+}
+
+function normalk () { 
+	if (gBrowser.contentDocument.location.toString().indexOf("http://www.google.com/reader") != 0) 
+		goDoCommand('cmd_scrollLineUp');
+}
+
+
+window.addEventListener("keypress", foolistener, true); 
+
 

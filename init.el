50342488355b61dcc31a858eb7fea5512d455d50
Larry D'anna <larry@oberon.(none)>
2008-09-05 16:28:50 -0400

    nxml keys

--- init.el
+++ init.el
@@ -1466,12 +1466,14 @@
   (quote
    (progn
      (define-key nxml-mode-map "\C-c/" 'nxml-finish-element)
-     (define-key nxml-mode-map "\C-\M-f" 'nxml-forward-element)
-     (define-key nxml-mode-map "\C-\M-b" 'nxml-backward-element)
-     (define-key nxml-mode-map "\M-f" 'nxml-forward-balanced-item)
-     (define-key nxml-mode-map "\M-b" '(lambda () 
-                                         (interactive)
-                                         (nxml-forward-balanced-item -1))))))
+     (define-key nxml-mode-map "\C-x\C-\M-f" 'nxml-forward-element)
+     (define-key nxml-mode-map "\C-x\C-\M-b" 'nxml-backward-element)
+     (define-key nxml-mode-map "\C-\M-f" 'nxml-forward-balanced-item)
+     (define-key nxml-mode-map "\C-\M-b" '(lambda () 
+                                            (interactive)
+                                            (nxml-forward-balanced-item -1)))
+     (define-key nxml-mode-map "\M-f" 'forward-word)
+     (define-key nxml-mode-map "\M-b" 'backward-word))))
 
 (eval-after-load 'sgml-mode
   (quote

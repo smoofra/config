4ff2c19d305cb75dc8fcace21953437dd35172e9
Larry D'Anna <larry@melkor.(none)>
2009-02-25 14:01:05 -0500

    get rid of ssystem, its ugly and stupid and i hate it

--- perl/Utils.pm
+++ perl/Utils.pm
@@ -6,7 +6,7 @@ package Utils;
 
 require Exporter;
 @ISA = qw(Exporter);
-@EXPORT = qw(slurp slurpdir croak ssystem);
+@EXPORT = qw(slurp slurpdir croak);
 
 
 
@@ -36,36 +36,6 @@ sub slurp {
   my $r = join '', <$fh>;
   close $fh;
   return $r;
-}
-
-sub ssystem  {
-  my $name = $_[0];
-  my @args = @_;
-  my $tmp = "/tmp/delicious_update$$";
-  my $pid = fork();
-  my ($fh ,$nh);
-  open $nh, "<", "/dev/null" or croak;
-  open $fh, ">", $tmp or croak;
-  my $fd = fileno $fh;
-  my $nd = fileno $nh;
-  if ($pid) {
-    close $fh;
-    waitpid $pid,0;
-    unless (0==$?) {
-      system qw/cat/, $tmp;
-      unlink $tmp;
-      die "$name failed\n";
-    }
-    unlink $tmp or croak;
-  } else {
-    POSIX::close(0);
-    POSIX::close(1);
-    POSIX::close(2);
-    POSIX::dup2($nd, 0);
-    POSIX::dup2($fd, 2);
-    POSIX::dup2($fd, 1); 
-    exec @args;    
-  }
 }
 
 

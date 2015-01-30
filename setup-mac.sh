2caad6be5c349ebd5ed1f9de09658236b14a20c4
Lawrence D'Anna <larry@elder-gods.org>
2015-01-30 15:54:40 -0800

    ssh stuff

--- setup-mac.sh
+++ setup-mac.sh
@@ -8,9 +8,15 @@ echo ". ~/.bashrc" >~/.bash_profile
 
 ( cd ~/config ; git config user.email larry@elder-gods.org )
 
+if ! [[ -e ~/apple_config ]]; then
+    git clone ssh://git@stash.sd.apple.com/~lawrence_danna/apple_config.git ~/apple_config
+fi
 
 
+if ! [[ -e ~/.ssh ]]; then
+    mkdir ~/.ssh
+    chmod 0700 ~/.ssh
+fi
 
-
-
+cat ~/config/ssh ~/apple_config/ssh  >~/.ssh/config
 

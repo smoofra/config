3e9e4c3caa34d6ebf0413dfaaff4607950c66ddd
Lawrence D'Anna <larry@elder-gods.org>
2019-05-12 13:13:59 -0700

    editors

--- init.el
+++ init.el
@@ -2169,7 +2169,7 @@
  '(octave-block-offset 4)
  '(package-selected-packages
    (quote
-    (xcscope dtrace-script-mode confluence vdirel go-mode yaml-mode cmake-mode archive-rpm fill-column-indicator php-mode a pbcopy rtags markdown-mode lua-mode dtrt-indent ack)))
+    (image+ xcscope dtrace-script-mode confluence vdirel go-mode yaml-mode cmake-mode archive-rpm fill-column-indicator php-mode a pbcopy rtags markdown-mode lua-mode dtrt-indent ack)))
  '(rtags-path (concat homebrew-path "/bin"))
  '(safe-local-variable-values
    (quote

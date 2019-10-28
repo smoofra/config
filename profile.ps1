272a7be42c63f6a5fcf9cfb1c6fae869db3f6704
Lawrence D'Anna <larry@elder-gods.org>
2019-10-28 15:49:14 -0700

    windows stuff

---
+++ profile.ps1
@@ -0,0 +1,29 @@
+
+Set-PSReadLineOption -EditMode Emacs
+
+# $env:Path += ";C:\\Program Files (x86)\\Microsoft Visual Studio\\Shared\\Python37_64"
+#
+#
+
+Function path() {
+ $hklm = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name Path).Path
+ $hkcu = (Get-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment' -Name Path).Path
+
+ echo "==== HKLM" 
+ echo $hklm.split(";")
+ echo ""
+
+ echo "==== HKCU"
+ echo $hkcu.split(";")
+ echo ""
+
+ echo "==== process"
+ echo $env:path.split(";")
+ echo ""
+}
+
+Function rerc() { 
+ . $profile.CurrentUserAllHosts
+}
+
+
---
+++ windows_notes.ps1
@@ -0,0 +1,71 @@
+
+# https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Get-WindowsCapability -Online | ? Name -like 'OpenSSH*'
+Start-Service sshd
+Set-Service -Name sshd -StartupType 'Automatic'
+# Confirm the Firewall rule is configured. It should be created automatically by setup. 
+Get-NetFirewallRule -Name *ssh*
+# There should be a firewall rule named "OpenSSH-Server-In-TCP", which should be enabled 
+Install-Module -Force OpenSSHUtils -Scope AllUsers
+Repair-AuthorizedKeyPermission authorized_keys
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
+
+# https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_overview
+
+# Save all the process' environment variables in CLIXML format.
+Get-ChildItem env: | Export-CliXml ./env-vars.clixml
+# Restore the previously saved env. variables.
+Import-CliXml ./env-vars.clixml | % { Set-Item "env:$($_.Name)" $_.Value }
+
+
+# https://github.com/PowerShell/PSReadLine
+Import-Module PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+
+
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+Set-ExecutionPolicy RemoteSigned
+
+
+cat $profile.CurrentUserAllHosts
+$env:Path += ";C:\Program Files (x86)\Vim\vim81"                                                                                                                                  
+
+cat \ProgramData\ssh\sshd_config
+SyslogFacility LOCAL0
+LogLevel  DEBUG
+
+RefreshEnv.cmd
+# doesn't seem to actually work
+[System.Environment]::GetEnvironmentVariable("Path","Machine")
+[System.Environment]::GetEnvironmentVariable("Path","User")
+[System.Environment]::SetEnvironmentVariable("Path",value, "Machine")
+
+$n = [Environment]::GetEnvironmentVariable("Path") + ";C:\Program Files\CMake\bin\"                                                                          
+[Environment]::SetEnvironmentVariable("Path", $n)                                                                                                            
+
+cmake -G Ninja `
+        "-DPYTHON_HOME=C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64" `
+        "-DLLVM_ENABLE_PROJECTS=clang;lldb;libcxx;libcxxabi" `
+        "-DSWIG_EXECUTABLE=C:\Program Files (x86)\swigwin-3.0.12\swig.exe" `
+        "C:\Users\temp\llvm-project\llvm"
+
+cd 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\' 
+cmd
+vcvars64.bat
+powershell
+Get-ChildItem env: | Export-CliXml ~/vcvars.clixml
+Import-CliXml ~/vcvars.clixml | % { Set-Item "env:$($_.Name)" $_.Value }
+
+
+
+New-Item -ItemType SymbolicLink -Path "Link" -Tarf "Target"
+
+choco install cmder 
+
+
+###### new host setup
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Start-Service sshd
+Set-Service -Name sshd -StartupType 'Automatic'

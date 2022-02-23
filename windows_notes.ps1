890a3473f7284c7a2ec80f6cf24cae4a99588010
Lawrence D'Anna <larry@elder-gods.org>
2022-02-23 11:49:15 -0500

    stuff

--- windows_notes.ps1
+++ windows_notes.ps1
@@ -72,3 +72,20 @@ choco install cmder
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
+
+
+###### winrm fuckery
+Set-Item wsman:\localhost\Client\TrustedHosts -value "*"
+Enable-PSRemoting -Force
+winrm quickconfig
+Set-NetConnectionProfile -NetworkCategory Private
+winrm get config winrm/config/service
+Enter-PSSession -ComputerName win7.foo.com  -Credential user
+
+$s = New-PSSession -ComputerName win7.foo.com -Credential user
+Enter-PSSession -Session $s
+Exit-PSSession
+
+
+ New-PSDrive -PSProvider FileSystem  -Root \\win7.foo.com\c -Credential user
+
